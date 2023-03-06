520f771acca1657f982646cd0d82bd917b793a47
Ryan Carroll <software@ryancarroll.io>
2023-03-05 21:07:15 -0500

    initial commit

---
+++ password_validator/main.py
@@ -0,0 +1,30 @@
+# Simple password validation.
+# Conditions: Characters > 8, Contains a number, and at least 1 uppercase
+
+password = input("Enter new password: ")
+
+result = {}
+
+if len(password) >= 8:
+    result["length"] = True
+else:
+    result["length"] = False
+
+digit = False
+for i in password:
+    if i.isdigit():
+        digit = True
+
+result["digits"] = digit
+
+uppercase = False
+for i in password:
+    if i.isupper():
+        uppercase = True
+
+result["case"] = uppercase
+
+if all(result.values()):
+    print("Strong password")
+else:
+    print("Weak password")
